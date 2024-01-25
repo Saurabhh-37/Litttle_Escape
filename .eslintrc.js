a121aeedfc42a20906118fc44e405fb77286dfca
Saurabh Hagawane <105903203+Saurabhh-37@users.noreply.github.com>
2024-01-25 10:53:02 +0000

    deploy

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,49 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["google", "plugin:react/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "extends": [
-        "google",
-        "plugin:react/recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "linebreak-style": 0,
+    quotes: [0, "double"],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    arraysInObjects: 0,
+    "object-curly-spacing": [2, "always"],
+    "max-len": ["error", { code: 700 }],
+    "comma-dangle": ["error", { functions: "ignore" }],
+    "no-unused-vars": 0,
+    "react/prop-types": 0,
+    "react/no-unescaped-entities": 0,
+    indent: ["error", 2],
+    "require-jsdoc": [
+      "error",
+      {
+        require: {
+          FunctionDeclaration: false,
+          MethodDefinition: false,
+          ClassDeclaration: false,
+          ArrowFunctionExpression: false,
+          FunctionExpression: false,
+        },
+      },
     ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+  },
+};
