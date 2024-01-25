1996b30264f914a2c53dde525a121421b2f44d30
Saurabh Hagawane <105903203+Saurabhh-37@users.noreply.github.com>
2024-01-25 11:39:36 +0000

    deploy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import { StrictMode } from "react";
+import reactDom from "react-dom";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+const root = reactDom.createRoot(rootElement);
 
 root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
 );
